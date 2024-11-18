4bc660d6e12460e85dbaa9427639d18abd9c326a
Junita Berglin <junita.berglin@gmail.com>
2024-11-18 15:47:15 +0100

    (feat): :sparkles: html and css done

--- LivLineWeb/Program.cs
+++ LivLineWeb/Program.cs
@@ -1,11 +1,36 @@
 using LivLineWeb.Services;
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5052")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers();
 builder.Services.AddScoped<SimplificationService>();
 
-app.UseStaticFiles();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "LivLine API v1");
+    });
+}
 
 app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapControllers();
 
 app.Run();
